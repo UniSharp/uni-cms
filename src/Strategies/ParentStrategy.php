c0047f7c591097891433a6aae60793cd384aa236
FreedomKnight <j26733514@gmail.com>
2018-01-02 19:35:14 +0800

    allow return null

--- src/Strategies/ParentStrategy.php
+++ src/Strategies/ParentStrategy.php
@@ -1,11 +1,14 @@
 <?php
-
 namespace UniSharp\UniCMS\Strategies;
 
 class ParentStrategy extends BaseStrategy
 {
     public function getResults()
     {
-        return $this->proxy->model->node->parent->page;
+        if ($this->proxy->model->node->parent) {
+            return $this->proxy->model->node->parent->page;
+        }
+        
+        return null;
     }
 }
