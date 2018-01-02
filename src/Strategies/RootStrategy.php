aa1702826ea3f3e22c5d86ae869f6b5c998da151
Sasaya <storyn26383@gmail.com>
2018-01-02 12:03:28 +0800

    fix bug

--- src/Strategies/RootStrategy.php
+++ src/Strategies/RootStrategy.php
@@ -6,7 +6,7 @@ class RootStrategy extends BaseStrategy
 {
     public function getResults()
     {
-        $walker = &$this->proxy->model->node->parent;
+        $walker = $this->proxy->model->node->parent;
 
         while ($walker->parent) {
             $walker = $walker->parent;
